c23459234e4f90fffbfee76fae1cb9edaa8f8eae
bagus <bagustriamukti998@gmail.com>
2021-06-09 05:13:48 +0700

    animasi

--- resources/views/document.blade.php
+++ resources/views/document.blade.php
@@ -2,14 +2,19 @@
     @include('layout.detailheader')
 @show
 <!--banner-->
-<section style="background-image: url(front/assets/images/bg-dokumen.png)" class="banner banner-2 d-flex align-items-center">
+<section style="background-image: url(front/assets/images/bg-dokumen.png)"
+         class="banner banner-2 d-flex align-items-center">
     <div class="container">
-        <div class="row">
+        <div class="row title-container">
             <div class="col-md-6">
                 <h1>Dokumen apa yang Anda cari?</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div id="container" class="col-md-6">
                 <form action="" class="mt-5">
-                    <div class="row row-cols-2 g-4">
-                        <div class="col">
+                    <div id="form-document" class="form-document">
+                        <div class="col-6">
                             <label for="document" class="form-label text-light">Jenis Dokumen</label>
                             <select id="document" class="form-control form-select select-ngo" name="state">
                                 <option value="AL">Momorandum Saling Pengertian</option>
@@ -18,7 +23,7 @@
                                 <option value="AL">Laporan</option>
                             </select>
                         </div>
-                        <div class="col">
+                        <div class="col-6">
                             <label for="tahun" class="form-label text-light">Tahun</label>
                             <select id="tahun" class="form-control form-select select-ngo" name="state">
                                 <option value="AL">2021</option>
@@ -27,31 +32,59 @@
                                 <option value="AL">2018</option>
                             </select>
                         </div>
-                    </div>
-                    <div class="row row-cols-1 mt-4">
-                        <div class="col">
+                        <div id="ngo-container" class="col-12">
                             <label for="ngo" class="form-label text-light">NGO</label>
-                            <select id="ngo" disabled class="form-control form-select select-ngo" name="state">
+                            <select id="ngo" disabled class="form-control form-select" name="state">
                                 <option value="AL">Rikolto</option>
                             </select>
                         </div>
+                        <div class="col-6">
+                            <button id="buttonSearch" type="button" class="btn btn-primary">Cari dokumen</button>
+                        </div>
                     </div>
-                    <button class="btn btn-primary mt-4">Cari dokumen</button>
                 </form>
             </div>
         </div>
     </div>
 </section>
 <!--end of banner-->
-
+<section class="document-found">
+    <div class="container py-5">
+        <h1 class="title">Hasil pencarian</h1>
+        <div class="documents mt-4">
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+            <div class="col document">
+                <img src="{{asset('front/assets/icons/pdf-icon.svg')}}" alt="Pdf">
+                <p class="mt-4">Dokumen MSP 2020 - 2023</p>
+            </div>
+        </div>
+    </div>
+</section>
 <!--footer-->
 @section('footer')
     @include('layout.footer')
 @show
 <!--footer end-->
-<script src="js/jquery-3.6.0.min.js"></script>
-<script src="js/select2.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
+
 
 <script>
     $(document).ready(function () {
@@ -63,6 +96,31 @@
 
         $('b[role="presentation"]').hide();
         $('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');
+
+
+        //animasi
+        $('#buttonSearch').click(function () {
+            const container = $('#container')
+            container.removeClass('col-md-5')
+            container.addClass('col-md-12')
+
+            const formDoc = $('#form-document')
+            formDoc.addClass('full')
+            formDoc.animate({
+                marginBottom: '50px',
+            }, 700)
+
+            const ngoContainer = $('#ngo-container')
+            ngoContainer.removeClass('col-12')
+            ngoContainer.addClass('col-6')
+
+            $('.banner-2').animate({
+                height: '150px',
+            }, 300)
+
+            $('.banner-2').addClass('found')
+            $('.document-found').show()
+        })
     });
 </script>
 </body>
